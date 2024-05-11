8da4e41a632e26a21e42f49b3e23fe780fad25fd
KhaldounB <khaldounbadreahf@gmail.com>
2024-05-11 18:53:53 +0300

    Connect Project With Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,20 @@
 import 'package:flutter/material.dart';
 
 // Package imports:
+import 'package:firebase_core/firebase_core.dart';
 import 'package:sizer/sizer.dart';
 
 // Project imports:
 import 'core/logic/singleton/singleton.dart';
 import 'core/logic/singleton/statics.dart';
+import 'firebase_options.dart';
 
 void main()async {
   WidgetsFlutterBinding.ensureInitialized();
   await singletone();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
